072eec45090bd65d7c6d56f90563e4cd580a1e8d
stan e <yi.hang@live.com>
2018-09-10 15:06:30 +0800

    SPA项目添加

---
+++ ABP/ABP_CORE_MPA.Web.Host/ABP_CORE_SPA.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Data;
+using ABP_CORE_MPA.Web.Host.Startup;
+
+namespace ABP_CORE_MPA.Web.Host
+{
+    internal class ABP_CORE_SPA : StatelessService
+    {
+        public ABP_CORE_SPA(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// 可选择性地替代以创建此服务实例的侦听器(如 TCP、http)。
+        /// </summary>
+        /// <returns>侦听器集合。</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext =>
+                    new KestrelCommunicationListener(serviceContext, "ServiceEndpoint", (url, listener) =>
+                    {
+                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
+
+                        return new WebHostBuilder()
+                                    .UseKestrel()
+                                    .ConfigureServices(
+                                        services => services
+                                            .AddSingleton<StatelessServiceContext>(serviceContext))
+                                    .UseContentRoot(Directory.GetCurrentDirectory())
+                                    .UseStartup<Startup.Startup>()
+                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                                    .UseUrls(url)
+                                    .Build();
+                    }))
+            };
+        }
+    }
+}
